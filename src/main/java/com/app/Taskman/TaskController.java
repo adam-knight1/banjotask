45426dff67fc4d6b3effb8faf1f28de2dfd3f771
adam-knight1 <amkn1ght@icloud.com>
2025-02-28 02:51:17 -0500

    added TaskController and Task model

--- src/main/java/com/app/Taskman/TaskController.java
+++ src/main/java/com/app/Taskman/TaskController.java
@@ -1,2 +1,38 @@
-package com.app.Taskman;public class TaskController {
+package com.app.Taskman;
+
+import com.app.Taskman.Task;
+import com.app.Taskman.TaskRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class TaskController {
+    private final TaskRepository taskRepository;
+
+    public TaskController(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    // Displaying list of tasks
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("tasks", taskRepository.findAll());
+        return "index";
+    }
+
+    // show form for a new task
+    @GetMapping("/add")
+    public String addTaskForm(Model model) {
+        model.addAttribute("task", new Task());
+        return "addTask";
+    }
+
+    // process new task submission
+    @PostMapping("/add")
+    public String addTaskSubmit(@ModelAttribute Task task) {
+        taskRepository.save(task);
+        return "redirect:/";
+    }
 }
+
